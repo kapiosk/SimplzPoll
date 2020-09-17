1404c0b35fca49166040b9d9b4f9ffd84d19e60d
kapiosk <dkknd@hotmail.com>
2020-09-17 14:26:20 +0000

    .

--- save.sh
+++ save.sh
@@ -5,4 +5,5 @@ git push
 # curl -u 'USER' https://api.github.com/user/repos -d '{"name":"REPO"}'
 # # Remember replace USER with your username and REPO with your repository/application name!
 # git remote add origin git@github.com:USER/REPO.git
-# git push origin master
+# git push --set-upstream origin master
+# git push
