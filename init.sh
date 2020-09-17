217d0ccfa56ca23fa6233600a159f2451e7c3b86
kapiosk <dkknd@hotmail.com>
2020-09-17 14:30:28 +0000

    .

---
+++ init.sh
@@ -0,0 +1,10 @@
+# git init
+# python3 -m venv venv
+# source venv/bin/activate
+# pip install flask
+# pip freeze > requirements.txt
+# curl -u 'USER' https://api.github.com/user/repos -d '{"name":"REPO"}'
+# # Remember replace USER with your username and REPO with your repository/application name!
+# git remote add origin git@github.com:USER/REPO.git
+# git push --set-upstream origin master
+# git push
--- save.sh
+++ save.sh
@@ -1,9 +1,3 @@
 git add .
 git commit -m .
 git push
-
-# curl -u 'USER' https://api.github.com/user/repos -d '{"name":"REPO"}'
-# # Remember replace USER with your username and REPO with your repository/application name!
-# git remote add origin git@github.com:USER/REPO.git
-# git push --set-upstream origin master
-# git push
