66d9441b7229486316e17bdaf3d408466e49e53f
kapiosk <dkknd@hotmail.com>
2020-09-18 11:04:41 +0000

    .

--- app.py
+++ app.py
@@ -7,3 +7,10 @@ from flask import Flask, render_template, request, session
 app = Flask(__name__)
 app.secret_key = config('APP_SECRET')
 app.SESSION_COOKIE_SECURE = True
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5501)
