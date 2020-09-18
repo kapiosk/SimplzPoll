2cc040a9e550b3389dd345cc2f079195a2980373
kapiosk <dkknd@hotmail.com>
2020-09-18 10:58:23 +0000

    .

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
 #!./venv/bin/python python3
 # -*- coding: utf-8 -*-
+
+from decouple import config
+from flask import Flask, render_template, request, session
+
+app = Flask(__name__)
+app.secret_key = config('APP_SECRET')
+app.SESSION_COOKIE_SECURE = True
